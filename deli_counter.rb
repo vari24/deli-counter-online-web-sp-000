ae2184dcbc7598704a1018ebb3d35ae477e07497
Sara Varisara <varisara.n@gmail.com>
2019-01-29 13:40:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,9 +21,9 @@ def take_a_number(katz_deli, customer)
 end
 
 def now_serving (katz_deli)
-  if katz_deli.empty?
+  if katz_deli.empty? == true
     puts "There is nobody waiting to be served!"
-  elsif
+  elsif katz_deli.empty? == false
     puts "Currently serving #{katz_deli.first}."
     katz_deli.shift
   end
