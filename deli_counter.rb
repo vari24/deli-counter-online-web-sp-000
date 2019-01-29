7d3aff617ad10ceb5f91593712a308b990a38c28
Sara Varisara <varisara.n@gmail.com>
2019-01-29 13:43:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,9 @@ def line(katz_deli)
  if katz_deli.length == 0
    puts "The line is currently empty."
  else
-   katz_line = katz_deli.collect {|line| "#{(katz_deli.index line).to_i + 1}. #{line}"}
-   puts "The line is currently: #{katz_line.join(" ")}"
+   puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}" 
+   end
  end
-end
 
 def take_a_number(katz_deli, customer)
   if katz_deli.empty? == true
