79c8a122064ae92326170789e5d1ef4d15f0efaa
Sara Varisara <varisara.n@gmail.com>
2019-01-29 13:26:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,10 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli == []
-  puts "The line is currently empty."
-end
+ if katz_deli.length == 0
+   puts "The line is currently empty."
+ else
+
 while count < katz_deli.count
   sentence = sentence + " #{count+1}. #{katz_deli[count]}"
   count += 1
