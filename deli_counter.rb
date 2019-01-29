9098bd91affa620de418903083ab6b532010d9b7
Sara Varisara <varisara.n@gmail.com>
2019-01-29 13:40:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ end
 def now_serving (katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
-  elsif katz_deli.empty? == false
+  elsif
     puts "Currently serving #{katz_deli.first}."
     katz_deli.shift
   end
